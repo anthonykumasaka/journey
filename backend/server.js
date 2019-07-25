ff7c09fe95bf9cba428b030aefc462550c52e290
Kumasaka <anthonykumasaka@gmail.com>
2019-07-24 22:59:21 -0700

    Get server port 5000 working

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,10 @@ mongoose
   .catch(err => console.log(err));
 
 
-app.get("/", (req, res) => res.send("Hello World")); 
+app.get("/", (req, res) => {
+  res.send("Hello World Ear error is for already running"); 
+}); 
 
 const port = process.env.PORT || 5000; 
 
-app.listen(port, () => console.log(`Server is runnin on port ${port}`));
+app.listen(port, () => {console.log(`Server is running on port ${port}`);});
